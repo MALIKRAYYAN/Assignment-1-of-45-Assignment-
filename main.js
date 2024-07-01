cc6949f42c06f6ca224720d44117176c7bf2c631
MALIKRAYYAN <159909430+MALIKRAYYAN@users.noreply.github.com>
2024-07-01 23:59:53 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+var Name = "Rayyan";
+console.log("hello Rayyan, would you like to learn some python today");
---
+++ main.ts
@@ -0,0 +1,2 @@
+let Name :string ="Rayyan"
+console.log("hello Rayyan, would you like to learn some python today")
